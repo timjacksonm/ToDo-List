3ff1dd81a84829ac139108957851ec843e25a075
Tim Jackson <timjacksonm@gmail.com>
2021-03-04 18:54:33 -0600

    created New ToDo form template to be used. installed npm flatpickr.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { display } from './display';
+import _ from 'flatpickr'
 
 // (function eventListeners() {
     
 //     const addProjectButton = document.getElementById('addProject');
 //     addProjectButton.addEventListener('click',  ()=>{});
 // })();
+const addProjectButton = document.getElementById('addProject');
+function run() {
+    const myInput = document.querySelector(".myInput");
+const fp = flatpickr(myInput, {});
+fp;
+}
+    addProjectButton.addEventListener('click',  ()=>{run()});
 display.appendAllTodos;
